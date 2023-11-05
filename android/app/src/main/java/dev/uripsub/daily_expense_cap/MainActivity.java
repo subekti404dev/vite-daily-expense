2f4972f4cd81f5493b5d47f483cd7242bd2a1862
Urip Subekti <urip.subekti13@gmail.com>
2023-11-05 23:20:52 +0700

    feat: add capacitor

---
+++ android/app/src/main/java/dev/uripsub/daily_expense_cap/MainActivity.java
@@ -0,0 +1,5 @@
+package dev.uripsub.daily_expense_cap;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'dev.uripsub.daily_expense_cap',
+  appName: 'Daily Expense',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
