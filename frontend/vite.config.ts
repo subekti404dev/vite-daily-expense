5c075414482039f95e93e45b2cbca98512d931d9
Urip Subekti <urip.subekti13@gmail.com>
2023-11-14 07:40:07 +0700

    feat: add  pwa

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA as vitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vitePWA({
+    registerType: 'autoUpdate',
+      filename: 'service-worker.js',
+      workbox: {
+        cleanupOutdatedCaches: true,
+        sourcemap: false,
+        globPatterns: ['**/*.{js,css,ico,png,svg}'],
+      },
+  })],
   server: {
     port: 3000,
   },
