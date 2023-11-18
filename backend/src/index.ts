7117f6b779c81578c08bd9ab9a790e69b1ef128c
Urip Subekti <urip.subekti13@gmail.com>
2023-11-19 00:11:22 +0700

    feat: add binary build

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import v1Routes from "./routes/v1";
 import dotenv from "dotenv";
+import path from "path";
 dotenv.config();
 require("cross-fetch/polyfill");
 
@@ -12,11 +13,16 @@ const port = process.env.PORT || 7000;
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/ping", (req: Request, res: Response) => {
   res.send("OK");
 });
 
 app.use("/v1", v1Routes);
+
+app.use(express.static("web"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(process.cwd(), "web", "index.html"));
+});
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
